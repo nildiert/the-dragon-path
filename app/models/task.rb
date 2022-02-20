4c72e94c0dfe7ad90d4b63b8e287ed91491e7f1b
Nildiert Jimenez Jaramillo <niljordan23@gmail.com>
2022-02-20 04:00:40 -0500

    Updated task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,11 +2,3 @@ class Task < ApplicationRecord
   belongs_to :base_task
   belongs_to :user
 end
-
-
-BaseTask.create(
-  title: "Album example",
-  description: "Abajo encontrarás una url, debes crear una pagina lo mas similar posible a la que esta en la url. No es necesario que sea responsive, pero seraá un plus para ti si así la quieres hacer.",
-  task_url: "https://getbootstrap.com/docs/5.1/examples/album/",
-  base_course: course
-)
