1126b177906703044d35908eef88bc6f4f031537
Nildiert Jimenez Jaramillo <niljordan23@gmail.com>
2022-02-20 03:50:08 -0500

    Fixed task table

--- db/migrate/20220215033153_create_tasks.rb
+++ db/migrate/20220215033153_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
-      t.references :course, null: false, foreign_key: true
+      # t.references :course, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
       t.string :link_github
       t.string :url
