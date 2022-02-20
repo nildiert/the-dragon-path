7e009dc14ccb0d931d8b10240ba5584ab657e19c
Nildiert Jimenez Jaramillo <niljordan23@gmail.com>
2022-02-20 01:45:01 -0500

    Fixed name of BaseCourseController

--- app/controllers/base_courses_controller.rb
+++ app/controllers/base_courses_controller.rb
@@ -1,4 +1,4 @@
-class CoursesController < ApplicationController
+class BaseCoursesController < ApplicationController
   before_action :set_course, only: %i[ show edit update destroy ]
 
   # GET /courses or /courses.json
